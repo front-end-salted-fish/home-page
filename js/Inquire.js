9f7cbc5e94fa78cc69c523a2e345a84e1508e469
曾曼青 <2192073703@qq.com>
2019-08-15 11:47:20 +0800

    Add files via upload

---
+++ js/Inquire.js
@@ -0,0 +1,248 @@
+var Load1 = $('#outerBox');
+//登陆框的移动
+function inquireMythird() {
+    var disX = 0; //横向距离
+    var disY = 0; //纵向距离
+    console.log(Load1);
+
+    Load1.get(0).onmousedown = function (ev) {
+        var oEvent = ev || event; //事件对象
+        disX = oEvent.pageX - Load1.get(0).offsetLeft; //鼠标位置减去 LoadBox的位置
+        disY = oEvent.pageY - Load1.get(0).offsetTop;
+        console.log(1);
+        oEvent.stopPropagation();
+        document.onmousemove = function (ev) {
+            console.log(2)
+            var oEvent = ev || event; //事件对象
+            var l = oEvent.pageX - disX;
+            var t = oEvent.pageY - disY;
+            if (l < 0) {
+                l = 0;
+            } else if (l > document.documentElement.clientWidth - Load1.get(0).offsetWidth) {
+                l = document.documentElement.clientWidth - Load1.get(0).offsetWidth;
+            }
+            if (t < 0) {
+                t = 0;
+            } else if (t > document.documentElement.clientHeight - Load1.get(0).offsetHeight) {
+                t = document.documentElement.clientHeight - Load1.get(0).offsetHeight;
+            }
+            Load1.get(0).style.left = l + 'px';
+            Load1.get(0).style.top = t + 'px';
+
+        };
+        document.onmouseup = function () {
+            document.onmousemove = null;
+            document.onmouseup = null; // LoadBox太小了，加在document上面大一点
+        };
+        // return false; //阻止默认事件
+       
+
+    }
+
+
+}
+inquireMythird();
+/* 提取公共的文本类表单项验证 */
+function inquireCommonInfo(reg, id) {
+    var inputText = document.getElementById(id);
+    var inputValue = inputText.value;
+    var inputSpan = document.getElementById(id + "Span");
+
+    if (inputValue == null || inputValue.length == 0) {
+        inputSpan.innerHTML = "不能为空！";
+        inputSpan.style.color = "red";
+        inputSpan.style.background = "none";
+        return false;
+    } else {
+        if (!reg.test(inputValue)) {
+            inputSpan.innerHTML = "格式有误！";
+            inputSpan.style.color = "red";
+            inputSpan.style.background = "none";
+            return false;
+        } else {
+            inputSpan.innerHTML = "√";
+            inputSpan.style.color = "white";
+            inputSpan.style.background = "rgb(50,200,100)";
+            inputSpan.style.padding = "2px 4px";
+            inputSpan.style.borderRadius = "15px";
+            return true;
+        }
+    }
+
+}
+
+/*验证手机号 */
+function checkNumber1() {
+    var reg = /^[1][0-9]{10}$/ig; /* 验证手机号 */
+    var id = 'phone';
+    return inquireCommonInfo(reg, id);
+}
+let $phone = $("#phone");
+let $checkBtn = $("#checkBtn");
+$phone.on("blur", () => {
+    checkNumber1();
+    $checkBtn.css({
+        "color": "#fff"
+    }); //增加边框样式
+    $checkBtn.removeClass("btn-add");
+});
+$phone.on("focus", () => {
+    $checkBtn.css({
+        "color": "green"
+    }); //增加边框样式
+    console.log(1);
+
+})
+//点击获取验证码，会出现一个60秒的定时器
+$checkBtn.on("click", () => {
+    var countdown = 60;
+
+    function settime(val) {
+        if (countdown == 0) {
+            val.removeAttr("disabled");
+            val.text("免费获取验证码");
+            countdown = 60;
+        } else {
+            val.attr({
+                disabled: "true"
+            });
+            val.text("重新发送(" + countdown + ")");
+            countdown--;
+            setTimeout(function () {
+                settime(val)
+            }, 1000)
+        }
+
+    }
+    settime($checkBtn);
+});
+/*验证学号和验证码 */
+function checkstudentNumber1(ID) {
+    var reg = /^\d*$/ig; /* 验证学号 */
+    var id = ID;
+    return inquireCommonInfo(reg, id);
+}
+let $studentNumber = $("#studentNumber");
+$studentNumber.on("blur", () => {
+    checkstudentNumber1('studentNumber');
+});
+let $checkCode = $("#checkCode");
+$checkCode.on("blur", () => {
+    checkstudentNumber1("checkCode");
+});
+
+/*点查询按钮会先检查是否都已填写*/
+(() => {
+    let $innerContainer = $(".innerContainer");
+    let $inquire = $('.inquire');
+    let $recruit = $("#recruit");
+    let $btn = $("#btn");
+    let $form = $(".container1 .form");
+    $btn.on("click", () => {
+        
+        let $formSpan = $(".container1 .formSpan");
+
+        $form.each((i) => {
+            if ($form.eq(i).val() == "") {
+                $formSpan.eq(i).text('不能为空！');
+                $formSpan.eq(i).css({
+                    "color": "red",
+                    "background": "none"
+                });
+
+                i++;
+            } else {
+                $innerContainer.eq(0).hide();
+                console.log($innerContainer.eq(0));
+
+                //根据后台请求接口判断要哪句话显示出来  
+                console.log($inquire.eq(0));
+
+                // $inquire.eq(1).show();
+            }
+        })
+        //发出Ajax请求
+        let $studentNumberval = $("#studentNumber").val();
+        let $phoneval = $("#phone").val();
+        // let usersData = [{
+        //     studentNumber: $studentNumberval
+        // }, {
+        //     phone: $phoneval
+        // }];
+        let usersData = {
+            studentNumber: $studentNumberval,
+            phone: $phoneval
+        };
+        console.log(usersData)
+
+        $.ajax({
+            type: 'POST',
+
+            data: JSON.stringify(usersData),
+
+            contentType: 'application/json',
+
+            dataType: 'json',
+
+            url: 'http://10.21.23.158:8888/home/studentEntry',
+
+            success: function (data) {
+
+                var datas = data;
+                console.log("成功了");
+
+
+                switch (datas.code) {
+                    case 0:
+                        alert(datas.msg);
+                        console.log(datas.object[0].status);
+                        
+                        if(datas.object[0].status == 0) {
+                            $recruit.text(datas.object[0].communityName+datas.object[0].departmentName);
+                            // $recruit.text(datas.object[0].departmentName);
+                                $inquire.eq(0).show();
+                        } else if (datas.object[0].status == 1) {
+                            $inquire.eq(1).show();
+                        } else if(datas.object[0].status == 2) {
+                            alert("有为空的地方！");
+                        }
+                           
+
+                        
+                        setTimeout(() => {
+                            // window.location.reload(); //3秒后刷新页面
+                        }, 3000);
+                        break;
+                    case 1:
+                        alert(datas.msg);
+                        break;
+                    default:
+                        alert(datas.msg);
+                        break;
+                }
+
+
+
+
+            },
+
+            error: function (e) {
+
+                alert("操作失败请重试");
+
+            }
+
+        });
+
+
+    })
+})();
+/*点击关闭按钮会关闭页面 */
+(() => {
+    let $close = $("#close");
+    let $outerBox = $("#outerBox");
+    $close.on("click", () => {
+        $outerBox.hide();
+    })
+})();
+// let $btn = $("#btn");
---
+++ js/communityRegister.js
@@ -0,0 +1,383 @@
+/* 提取公共的文本类表单项验证 */
+function commonInfo(reg, id) {
+    var inputText = document.getElementById(id);
+    var inputValue = inputText.value;
+    var inputSpan = document.getElementById(id + "Span");
+
+    if (inputValue == null || inputValue.length == 0) {
+        inputSpan.innerHTML = "不能为空！";
+        inputSpan.style.color = "red";
+        inputSpan.style.background = "none";
+        return false;
+    } else {
+        if (!reg.test(inputValue)) {
+            inputSpan.innerHTML = "格式有误！";
+            inputSpan.style.color = "red";
+            inputSpan.style.background = "none";
+            return false;
+        } else {
+            inputSpan.innerHTML = "√";
+            inputSpan.style.color = "white";
+            inputSpan.style.background = "rgb(50,200,100)";
+            inputSpan.style.padding = "2px 4px";
+            inputSpan.style.borderRadius = "15px";
+            return true;
+        }
+    }
+
+}
+/* 验证社团名字和管理员名字还有所属院名称*/
+function checkUsername(ID) {
+    var reg = /^[a-zA-Z\u4e00-\u9fa5]{1}[a-zA-Z0-9_\u4e00-\u9fa5]{1,19}$/ig; /* 既支持中文，又支持英文字符，不能以数字开头。限制为2-20个字符 */
+    var id = ID;
+    return commonInfo(reg, id);
+}
+var communityName = document.getElementById("communityName");
+var userName = document.getElementById("userName");
+var academicInstitution = document.getElementById("academicInstitution");
+communityName.addEventListener("blur", () => {
+    checkUsername("communityName");
+})
+userName.addEventListener("blur", () => {
+    checkUsername("userName");
+})
+academicInstitution.addEventListener("blur", () => {
+    checkUsername("academicInstitution");
+})
+/*验证学号 */
+function checkstudentNumber() {
+    var reg = /^\d*$/ig; /* 验证学号 */
+    var id = 'studentNumber1';
+    return commonInfo(reg, id);
+}
+let $studentNumber1 = $("#studentNumber1");
+$studentNumber1.on("blur", () => {
+    checkstudentNumber();
+});
+/*验证手机号 */
+function checkNumber3() {
+    var reg = /^[1][0-9]{10}$/ig; /* 验证手机号 */
+    var id = 'Number1';
+    return commonInfo(reg, id);
+}
+let $Number1 = $("#Number1");
+$Number1.on("blur", () => {
+    checkNumber3();
+});
+/*验证密码*/
+function checkPwd3() {
+    var reg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/ig; /*密码采用数字、字母、特殊字符且长度为8-20位*/
+    var id = 'passWord';
+    return commonInfo(reg, id);
+}
+let $passWord = $("#passWord");
+let $indicate = $("#indicate");
+$passWord.on("focus", () => {
+    $indicate.show();
+});
+$passWord.on("blur", () => {
+    checkPwd3();
+    $indicate.hide();
+});
+/*点注册按钮会先检查是否都已填写，之后会出现等待审核的图片（再加一些判断*/
+(() => {
+    const $innerImg = $("#innerImg");
+    const $formBox = $(".formBox");
+    let $btn1 = $("#btn1");
+    let $form = $(".Registercontainer1 .form");
+    // let $communityLabelIdval; //社团级别
+
+    // $("#level").on('change', function() {
+    //     // console.log(this.innerHTML)
+    //     $communityLabelIdval =  $("#level").get(0).selectedIndex;
+    // })
+    $btn1.on("click", () => {
+
+        let $formSpan = $(".Registercontainer1 .formSpan");
+
+        $form.each((i) => {
+            if ($form.eq(i).val() == "") {
+                $formSpan.eq(i).text('不能为空！');
+                $formSpan.eq(i).css({
+                    "color": "red",
+                    "background": "none"
+                });
+
+                i++;
+            } else {
+                $innerImg.css({
+                        'display': "block"
+                    }
+
+                )
+
+                $formBox.hide();
+            }
+        });
+        //点击会发出Ajax请求
+        let $adminNameval = $("#userName").val();
+        let $studentNumberval = $("#studentNumber1").val();
+        let $phoneval = $("#Number1").val();
+        let $passwordval = $("#passWord").val();
+        let $communityNameval = $("#communityName").val();
+        let $communityLabelIdval = $("#level").get(0).selectedIndex+1; //社团级别
+        let $academyNameval = $("#academicInstitution").val();
+        let $srcval = $("#prevUpload li img").attr("data-image"); //要上传整个图片的文件，不只是url
+        let usersData = {
+            adminName: $adminNameval,
+            studentNumber: $studentNumberval,
+            phone: $phoneval,
+            password: $passwordval,
+            communityName: $communityNameval,
+            communityLabelId: $communityLabelIdval,
+            academyName: $academyNameval,
+            src: $srcval
+        };
+
+        console.log(usersData)
+        console.log($adminNameval)
+        if ($adminNameval === ""||
+        $studentNumberval === ""||
+        $phoneval === ""||
+        $passwordval === ""||
+        $communityNameval === ""||
+        $communityLabelIdval === ""||
+        $academyNameval === ""||
+        $srcval === "") {
+            $innerImg.hide();
+            $formBox.show();
+            alert("还有信息没有填写哦~")
+        } else {
+            console.log("进来了")
+            $.ajax({
+
+                type: 'POST',
+    
+                data: JSON.stringify(usersData),
+    
+                contentType: 'application/json',
+    
+                dataType: 'json',
+    
+                url: 'http://10.21.23.158:8888/register/registerCommunity',
+    
+                success: function (data) {
+    
+                    var datas = data;
+                    console.log("成功了");
+    
+    
+                    switch (datas.code) {
+                        case 0:
+                            alert(datas.msg);
+                            setTimeout(() => {
+                                window.location.reload(); //3秒后重新刷新一下页面
+                            }, 1000);
+                            break;
+    
+                        default:
+                            alert(datas.msg);
+                            break;
+                    }
+    
+    
+    
+    
+                },
+    
+                error: function (e) {
+    
+                    alert("操作失败请重试");
+    
+                }
+    
+            });
+        }
+        
+    })
+})();
+/*点击关闭按钮会关闭页面 */
+(() => {
+    let $close1 = $("#close1");
+    let $outerBox1 = $("#outerBox1");
+    $close1.on("click", () => {
+        $outerBox1.hide();
+    })
+})();
+//眼睛睁开和闭上
+var eye = document.getElementById("eye");
+var pwd = document.getElementById("passWord");
+
+function showhide1() {
+
+    if (pwd.type == "password") {
+        pwd.type = "text";
+        eye.className = 'fa fa-eye-slash'
+    } else {
+        pwd.type = "password";
+        eye.className = 'fa fa-eye'
+    }
+}
+
+/*创建Web Uploader实例 */
+// 初始化Web Uploader
+// var $list = $('.list');
+// var uploader = WebUploader.create({
+
+//     // 选完文件后，是否自动上传。
+//     auto: true,
+
+//     // swf文件路径
+//     swf: '../webuploader-0.1.5/Uploader.swf',
+
+//     // 文件接收服务端。
+//     server: 'http://10.21.23.158:8888/uploadImage',
+
+//     // 选择文件的按钮。可选。
+//     // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+//     pick: '#filePicker',
+
+//     // 只允许选择图片文件。
+//     accept: {
+//         title: 'Images',
+//         extensions: 'gif,jpg,jpeg,bmp,png',
+//         mimeTypes: 'image/*'
+//     }
+// });
+/*监听fileQueued事件，通过uploader.makeThumb来创建图片预览图。
+PS: 这里得到的是Data URL数据，IE6、IE7不支持直接预览。可以借助FLASH或者服务端来完成预览。 */
+// 当有文件添加进来的时候
+// uploader.on('fileQueued', function (file) {
+//     var $li = $(
+//             '<div id="' + file.id + '" class="file-item thumbnail">' +
+//             '<img>' +
+//             '</div>'
+//         ),
+//         $img = $li.find('img');
+
+
+//     // $list为容器jQuery实例
+//     $list.append($li);
+
+//     // 创建缩略图
+//     // 如果为非图片文件，可以不用调用此方法。
+//     // thumbnailWidth x thumbnailHeight 为 100 x 100
+//     var thumbnailWidth = 50,
+//         thumbnailHeight = 50;
+//     uploader.makeThumb(file, function (error, src) {
+//         if (error) {
+//             $img.replaceWith('<span>不能预览</span>');
+//             return;
+//         }
+
+//         $img.attr('src', src);
+//     }, thumbnailWidth, thumbnailHeight);
+// });
+// /*当文件上传过程中, 上传成功，上传失败，上传完成都分别对应uploadProgress, uploadSuccess, uploadError, uploadComplete事件。 */
+// // 文件上传过程中创建进度条实时显示。
+// uploader.on('uploadProgress', function (file, percentage) {
+//     var $li = $('#' + file.id),
+//         $percent = $li.find('.progress span');
+
+//     // 避免重复创建
+//     if (!$percent.length) {
+//         $percent = $('<p class="progress"><span></span></p>')
+//             .appendTo($li)
+//             .find('span');
+//     }
+
+//     $percent.css('width', percentage * 100 + '%');
+// });
+
+// // 文件上传成功，给item添加成功class, 用样式标记上传成功。
+// uploader.on('uploadSuccess', function (file) {
+//     $('#' + file.id).addClass('upload-state-done');
+// });
+
+// // 文件上传失败，显示上传出错。
+// uploader.on('uploadError', function (file) {
+//     var $li = $('#' + file.id),
+//         $error = $li.find('div.error');
+
+//     // 避免重复创建
+//     if (!$error.length) {
+//         $error = $('<div class="error"></div>').appendTo($li);
+//     }
+
+//     $error.text('上传失败');
+// });
+
+// // 完成上传完了，成功或者失败，先删除进度条。
+// uploader.on('uploadComplete', function (file) {
+//     $('#' + file.id).find('.progress').remove();
+// });
+
+//上传图片
+
+// function filePrevImg(files) {
+//     //检测浏览器是否支持FileReader对象
+//     if (typeof FileReader == "undefined") {
+//         alert("您的浏览器不支持FileReader对象！");
+//     }
+//     var strHtml = "";
+//     for (var intI = 0; intI < files.length; intI++) {
+//         var tmpFile = files[intI];
+//         var reader = new FileReader(); //每循环一次都要重新new一个FileReader实例
+//         reader.readAsDataURL(tmpFile);
+//         reader.οnlοad = function (e) {
+//             alert(e.target.result);
+//             strHtml += "<span>";
+//             strHtml += "<img src='" + e.target.result + "' alt=''/>";
+//             strHtml += "</span>";
+//             $("#prevUpload").html("<li>" + strHtml + "</li>") ;
+//         };
+//     }
+// }
+$("#fileUpload").on("change", function () {
+    let file = this.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file); //调用自带方法进行转换 
+    
+    reader.onload = function (e) {
+    // this.result 就是base64
+    let finalImage = e.target.result.substring(e.target.result.indexOf(',') + 1);
+    let fileImage = {data:finalImage};
+
+    console.log(fileImage);
+    // $(".global-img").attr("src", this.result);
+    // 存储并获取url生成
+    $.ajax({
+    type: "post",
+    url: 'http://10.21.23.158:8888/apply/uploadImage',
+    // data: fileImage,
+    data:fileImage,
+    
+    
+    dataType: "json",
+    success: function (data) {
+    // 添加到新列表
+    console.log(data);
+    
+    // let datas = data;
+    let str = '';
+    str += "<li><img src="+e.target.result+" data-image="+data.object+"></li>";
+    $("#prevUpload").html(str);
+    // createApplyTr(response.object.src, fileName);
+    }
+    });
+    
+    }
+    });
+//选择校级时，所属院名称不可编辑
+let $level = $("#level");
+$level.on("blur", () => {
+    let $levelSelected = $("#level option:selected").text();
+    let $academicInstitution = $("#academicInstitution");
+    if ($levelSelected == '校级') {
+        $academicInstitution.attr({
+            disabled: "true"
+        });
+    } else {
+        $academicInstitution.removeAttr("disabled");
+    }
+});
